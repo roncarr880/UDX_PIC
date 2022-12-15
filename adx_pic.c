85e8ecc7b044e5c542c0e297c96685460ee399e5
roncarr880 <roncarr880@gmail.com>
2022-12-14 21:55:18 -0500

    Add files via upload

--- adx_pic.c
+++ adx_pic.c
@@ -7,12 +7,43 @@ char _temp;
 char _temp2;
 char _eedata;
 
+/* pre-calculated si5351 solutions in eeprom */
+/* 1st 2 values are P3, last two are P2, 5th is P1 LSB */
+/* solution is with 3 fractional bits, so audio tone * 8 is the offset to add in */
+/* 6th value must be zero for the algorithms to work */
+extern char s40ft8[] = { 0x36, 0x7F, 0x00, 0x0D, 0xD8, 0x00, 0x0C, 0x58 };
+extern char s40ft4[] = { 0x36, 0x7F, 0x00, 0x0D, 0xC9, 0x00, 0x01, 0xC9 };
+extern char s40js8[] = { 0x36, 0x7F, 0x00, 0x0D, 0xDA, 0x00, 0x1C, 0x5A };
+extern char s40wsp[] = { 0x36, 0x7F, 0x00, 0x0D, 0xC3, 0x00, 0x32, 0xC3 };
+extern char s20ft8[] = { 0x65, 0xBB, 0x00, 0x0E, 0xE3, 0x00, 0x17, 0x2F };
+extern char s20ft4[] = { 0x65, 0xBB, 0x00, 0x0E, 0xE5, 0x00, 0x07, 0x39 };
+extern char s20js8[] = { 0x65, 0xBB, 0x00, 0x0E, 0xE4, 0x00, 0x2E, 0x74 };
+extern char s20wsp[] = { 0x65, 0xBB, 0x00, 0x0E, 0xE9, 0x00, 0x57, 0xCD };
+extern char s15ft8[] = { 0x98, 0x99, 0x00, 0x0E, 0xDB, 0x00, 0x63, 0x9D };
+extern char s15ft4[] = { 0x98, 0x99, 0x00, 0x0E, 0xE9, 0x00, 0x19, 0xBF };
+extern char s15js8[] = { 0x98, 0x99, 0x00, 0x0E, 0xDC, 0x00, 0x48, 0x04 };
+extern char s15wsp[] = { 0x98, 0x99, 0x00, 0x0E, 0xDF, 0x00, 0x85, 0x39 };
+extern char s10ft8[] = { 0xCB, 0x76, 0x00, 0x0E, 0xD7, 0x00, 0xAF, 0xE6 };
+extern char s10ft4[] = { 0xCB, 0x76, 0x00, 0x0E, 0xE8, 0x00, 0x1D, 0x90 };
+extern char s10js8[] = { 0xCB, 0x76, 0x00, 0x0E, 0xD8, 0x00, 0x61, 0x70 };
+extern char s10wsp[] = { 0xCB, 0x76, 0x00, 0x0E, 0xDF, 0x00, 0x81, 0xB6 };
+
+
+extern char dividers[4] = { 112,60,40,30 };
+
 extern char hello[] = {'H','e','l','l','o','\r','\n',0};
 
 
 /* char sec4;         /* 1/4 seconds counts, !!! not sure will need this*/
 
-char msec;         /* about 1ms counts */
+char msec;           /* about 1ms counts */
+char mode;
+char band;
+char solution[8];    /* si5351 freq solution to send to si5351 */
+
+/* have functions get_rx_solution, get_tx_solution which can use the rx_solution then add some values
+   send_solution( reset ), will want reset for rx, not for tx, also will need to set the dividers for rx,
+   perhaps can precalc that too  */
 
 
 main(){
@@ -31,7 +62,7 @@ static char i;
     while( c = hello[i++] ) putch( c );
 
     while(1){               /* generate some I2C traffic for testing */
-       i2start( SI5351 );   /* will errors hang everything ? */
+       i2start( SI5351 );
        i2send( 3 );
        i2send( 0xff );
        i2stop();
@@ -47,11 +78,14 @@ init(){
 
   /* OSCCON = 0x72;              /* !!! testing 8 mhz internal clock */
 
-  LATC = 0;                      /* just in case the I2C pins get set to outputs, have them drive low */
   
   /* init vars that need values */
+  mode = 3;       /* wspr */
+  band = 0;       /* 40 meters */
 
-  /* set up the uart for debug, conflicts with B6 and B7 led control so disable this code when start driving the LED's */
+
+  /* set up the uart for debug, conflicts with B6 and B7 led control
+     so disable this code when start driving the LED's */
    SPBRG = 51;           /* 9600 baud */
    TXSTA = 0x20;         /* enable tx, slow baud rates */
    #asm
@@ -60,7 +94,7 @@ init(){
      bsf RCSTA,SPEN      ; enable Uart
    #endasm
 
-   /* msec = 0;             /* can't have two asm blocks in a row, compiler error, add dummy statement */
+     /* can't have two asm blocks in a row, compiler error, add some statements here */
 
   /* setup port I/O */
    LATB = 0;
@@ -154,7 +188,7 @@ putch( char c ){             /* send a character on the serial line */
 
 i2begin(){
 
-   SSPCON1 = 0;         /* clear any errors by turning off */
+   SSPCON1 = 0;         /* clear any errors by turning off ? */
    delay(1);
    SSPCON1 = 0x28;      /*  00101000 enable and master mode */
    #asm
